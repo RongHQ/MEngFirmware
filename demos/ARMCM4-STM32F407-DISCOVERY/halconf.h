6a1213d42875e92b4591c7f1120216649735d010
yunfei <yunfei@yunfei-ThinkPad-X220.(none)>
2014-06-06 17:12:22 -0400

    2014-Jun-6

--- demos/ARMCM4-STM32F407-DISCOVERY/halconf.h
+++ demos/ARMCM4-STM32F407-DISCOVERY/halconf.h
@@ -273,7 +273,7 @@
  *          default configuration.
  */
 #if !defined(SERIAL_DEFAULT_BITRATE) || defined(__DOXYGEN__)
-#define SERIAL_DEFAULT_BITRATE      921600
+#define SERIAL_DEFAULT_BITRATE   9600  /*921600 Previous defined Baud rate by Hongqian*/
 #endif
 
 /**
